11a9cdf15a6e3560c21131cc7470d63425895ee6
usiddiki97 <usiddiki97@gmail.com>
2023-04-19 19:33:10 -0400

    Finish helpers,layouts,partials

---
+++ W7D2/99 Cats II - Helpers_Layouts_Partials /app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+    def welcome_email(user)
+        @user = user
+        @url = "http://localhost:3000/session/new"
+        mail(to: @user.username, subject: "Welcome to 99 Cats!" )
+    end
+end
