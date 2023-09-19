458692c25b46158f64420dcdbde5e7de4a79eed9
usiddiki97 <usiddiki97@gmail.com>
2023-09-18 20:03:40 -0400

    finish

--- W9D5/Vanilla DOM Manipulation/script.js
+++ W9D5/Vanilla DOM Manipulation/script.js
@@ -19,13 +19,42 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const addLi = (e) => {
+    e.preventDefault();
 
+    const inputElement = document.querySelector(".favorite-input");
+    const inputValue = inputElement.value;
+    
+    const li = document.createElement("li");
+    li.textContent = inputValue;
+
+    document.getElementById("sf-places").appendChild(li);
+    inputElement.value = "";
+  }
+
+  document.querySelector("form").addEventListener("submit", addLi);
 
 
   // adding new photos
 
   // --- your code here!
+  const hiddenForm = document.querySelector(".photo-form-container");
 
+  document.querySelector("button").addEventListener("click", () => {
+    hiddenForm.classList.toggle("hidden");
+  })  
 
+  hiddenForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const imageURL = document.querySelector(".photo-url-input");
+    const img = document.createElement("img");
+    img.src = imageURL.value;
+
+    const li = document.createElement("li");
+    li.appendChild(img);
+    document.querySelector(".dog-photos").appendChild(li);
+    imageURL.value = "";
+  })
 
 });
